786bd2f9f53c9014fd3ac3124c27c7524dfe2234
ElviraMirsaitova <amalteya7@gmail.com>
2024-08-01 00:08:06 +0300

    Add files via upload

---
+++ Modul_1_2.py
@@ -0,0 +1,5 @@
+zadaniy_gotovy = 12
+chasov_potracheno = 1.5
+name_of_course = "Python"
+vremya_na_zadanie = chasov_potracheno / zadaniy_gotovy
+print('Курс:',name_of_course,', всего задач:',zadaniy_gotovy,', затрачено часов:',chasov_potracheno,', среднее веремя выполнения:',vremya_na_zadanie)
